2ea22f2d84b42eea1daba3042b001aba477b0a2a
key kim <keyhyuk.kim@gmail.com>
2019-03-10 07:57:33 +0000

    Add more example of abstract factory

--- Test/AbstractFactoryTest.cs
+++ Test/AbstractFactoryTest.cs
@@ -18,8 +18,14 @@ namespace hwapp
 			var instance = this.nonsan.GetInstance(typeof(CyberSoldier), "김사이버", "SW개발", 32, "대한민국");
 			instance.ShowMeYourTag();
 			
+			var instance1 = this.nonsan.GetInstance(typeof(CyberSoldier));
+			instance1.ShowMeYourTag();
+			
 			var instance2 = this.weapon.GetInstance(typeof(Rifie), "K-2", "대한민국");
 			instance2.ShowMeYourTag();
+			
+			var instance3 = this.weapon.GetInstance(typeof(Rifie));
+			instance3.ShowMeYourTag();
 		}
 	}
 }
